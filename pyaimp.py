2b1f6455d00db4787ef4b9a8a40399f6b8ad2c3b
EpocDotFr <contact@epoc.fr>
2016-12-29 19:24:23 +0100

    So many thing

--- pyaimp.py
+++ pyaimp.py
@@ -1,7 +1,9 @@
 from enum import Enum
+from win32con import WM_USER
+from mmapfile import mmapfile
 import win32gui
 import win32api
-from win32con import WM_USER
+import struct
 
 __version__ = '0.1.0'
 
@@ -9,6 +11,9 @@ __all__ = [
     'PlayerState',
     'Client'
 ]
+
+AIMPRemoteAccessClass = 'AIMP2_RemoteInfo'
+AIMPRemoteAccessMapFileSize = 2048
 
 # -----------------------------------------------------
 # Message types to send to AIMP
@@ -42,6 +47,8 @@ AIMP_RA_CMD_BASE = 10
 AIMP_RA_CMD_REGISTER_NOTIFY = AIMP_RA_CMD_BASE + 1
 AIMP_RA_CMD_UNREGISTER_NOTIFY = AIMP_RA_CMD_BASE + 2
 
+WM_AIMP_COPYDATA_ALBUMART_ID = 0x41495043
+
 AIMP_RA_CMD_PLAY = AIMP_RA_CMD_BASE + 3
 AIMP_RA_CMD_PLAYPAUSE = AIMP_RA_CMD_BASE + 4
 AIMP_RA_CMD_PAUSE = AIMP_RA_CMD_BASE + 5
@@ -63,7 +70,7 @@ AIMP_RA_CMD_VISUAL_START = AIMP_RA_CMD_BASE + 20
 AIMP_RA_CMD_VISUAL_STOP = AIMP_RA_CMD_BASE + 21
 
 # -----------------------------------------------------
-# Notifications
+# Events
 
 AIMP_RA_NOTIFY_BASE = 0
 
@@ -83,33 +90,45 @@ class Client:
     hwnd = None
 
     def __init__(self):
-        self.hwnd = win32gui.FindWindow('AIMP2_RemoteInfo', None)
+        self.hwnd = win32gui.FindWindow(AIMPRemoteAccessClass, None)
 
         if not self.hwnd:
             raise RuntimeError('Unable to find the AIMP instance. Are you sure it is running?')
-
-    # -----------------------------------------------------
-    # Properties
 
     def _get_prop(self, prop_id):
         return win32api.SendMessage(self.hwnd, WM_AIMP_PROPERTY, prop_id | AIMP_RA_PROPVALUE_GET, 0)
 
     def _set_prop(self, prop_id, value):
-        return win32api.SendMessage(self.hwnd, WM_AIMP_PROPERTY, prop_id | AIMP_RA_PROPVALUE_SET, value)
+        win32api.SendMessage(self.hwnd, WM_AIMP_PROPERTY, prop_id | AIMP_RA_PROPVALUE_SET, value)
+
+    def _send_command(self, command_id, parameter=None):
+        return win32api.SendMessage(self.hwnd, WM_AIMP_COMMAND, command_id, parameter)
+
+    def get_current_track_infos(self):
+        mapped_file = mmapfile(None, AIMPRemoteAccessClass, MaximumSize=AIMPRemoteAccessMapFileSize)
+
+        mapped_file_content = mapped_file.read(AIMPRemoteAccessMapFileSize)
+
+        mapped_file.close()
+
+        print(struct.unpack('I ? I I I I I I I I I I I I I I', mapped_file_content))
+
+    # -----------------------------------------------------
+    # Properties
 
     def get_version(self):
         version = self._get_prop(AIMP_RA_PROPERTY_VERSION)
 
         if not version:
-            raise Exception('Invalid version')
+            return None
 
-        return (win32api.HIWORD(version), win32api.LOWORD(version))
+        return ('{:.2f}'.format(win32api.HIWORD(version) / 100), win32api.LOWORD(version))
 
     def get_player_position(self):
         return self._get_prop(AIMP_RA_PROPERTY_PLAYER_POSITION)
 
     def set_player_position(self, position):
-        return self._set_prop(AIMP_RA_PROPERTY_PLAYER_POSITION, position)
+        self._set_prop(AIMP_RA_PROPERTY_PLAYER_POSITION, position)
 
     def get_current_track_duration(self):
         return self._get_prop(AIMP_RA_PROPERTY_PLAYER_DURATION)
@@ -121,39 +140,104 @@ class Client:
         return self._get_prop(AIMP_RA_PROPERTY_VOLUME)
 
     def set_volume(self, volume):
-        return self._set_prop(AIMP_RA_PROPERTY_VOLUME, volume)
+        self._set_prop(AIMP_RA_PROPERTY_VOLUME, volume)
 
     def is_muted(self):
         return bool(self._get_prop(AIMP_RA_PROPERTY_MUTE))
 
     def set_muted(self, muted):
-        return self._set_prop(AIMP_RA_PROPERTY_MUTE, int(muted))
+        self._set_prop(AIMP_RA_PROPERTY_MUTE, int(muted))
 
     def is_track_repeated(self):
         return bool(self._get_prop(AIMP_RA_PROPERTY_TRACK_REPEAT))
 
     def set_track_repeated(self, repeat):
-        return self._set_prop(AIMP_RA_PROPERTY_TRACK_REPEAT, int(repeat))
+        self._set_prop(AIMP_RA_PROPERTY_TRACK_REPEAT, int(repeat))
 
     def is_shuffled(self):
         return bool(self._get_prop(AIMP_RA_PROPERTY_TRACK_SHUFFLE))
 
     def set_shuffled(self, shuffled):
-        return self._set_prop(AIMP_RA_PROPERTY_TRACK_SHUFFLE, int(shuffled))
+        self._set_prop(AIMP_RA_PROPERTY_TRACK_SHUFFLE, int(shuffled))
 
     def is_recording(self):
         return bool(self._get_prop(AIMP_RA_PROPERTY_RADIOCAP))
 
     def set_recording(self, recording):
-        return self._set_prop(AIMP_RA_PROPERTY_RADIOCAP, int(recording))
+        self._set_prop(AIMP_RA_PROPERTY_RADIOCAP, int(recording))
 
     def is_visualization_fullscreen(self):
         return bool(self._get_prop(AIMP_RA_PROPERTY_VISUAL_FULLSCREEN))
 
     def set_visualization_fullscreen(self, visualization_fullscreen):
-        return self._set_prop(AIMP_RA_PROPERTY_VISUAL_FULLSCREEN, int(visualization_fullscreen))
+        self._set_prop(AIMP_RA_PROPERTY_VISUAL_FULLSCREEN, int(visualization_fullscreen))
 
     # -----------------------------------------------------
     # Commands
 
-    
+    def play(self):
+        self._send_command(AIMP_RA_CMD_PLAY)
+
+    def play_pause(self):
+        self._send_command(AIMP_RA_CMD_PLAYPAUSE)
+
+    def pause(self):
+        self._send_command(AIMP_RA_CMD_PAUSE)
+
+    def stop(self):
+        self._send_command(AIMP_RA_CMD_STOP)
+
+    def next(self):
+        self._send_command(AIMP_RA_CMD_NEXT)
+
+    def prev(self):
+        self._send_command(AIMP_RA_CMD_PREV)
+
+    def next_visualization(self):
+        self._send_command(AIMP_RA_CMD_VISUAL_NEXT)
+
+    def prev_visualization(self):
+        self._send_command(AIMP_RA_CMD_VISUAL_PREV)
+
+    def quit(self):
+        self._send_command(AIMP_RA_CMD_QUIT)
+
+    def add_files(self):
+        self._send_command(AIMP_RA_CMD_ADD_FILES)
+
+    def add_folders(self):
+        self._send_command(AIMP_RA_CMD_ADD_FOLDERS)
+
+    def add_playlists(self):
+        self._send_command(AIMP_RA_CMD_ADD_PLAYLISTS)
+
+    def add_url(self):
+        self._send_command(AIMP_RA_CMD_ADD_URL)
+
+    def open_files(self):
+        self._send_command(AIMP_RA_CMD_OPEN_FILES)
+
+    def open_folders(self):
+        self._send_command(AIMP_RA_CMD_OPEN_FOLDERS)
+
+    def open_playlists(self):
+        self._send_command(AIMP_RA_CMD_OPEN_PLAYLISTS)
+
+    def get_album_image(self):
+        album_art = self._send_command(AIMP_RA_CMD_GET_ALBUMART)
+
+        if not album_art:
+            return None
+
+        # TODO
+
+    def start_visualization(self):
+        self._send_command(AIMP_RA_CMD_VISUAL_START)
+
+    def stop_visualization(self):
+        self._send_command(AIMP_RA_CMD_VISUAL_STOP)
+
+    # -----------------------------------------------------
+    # Events
+
+    # TODO
